aa1cce08580959fa1ec455817611d9aaf1846881
METR1CKA <ferchosalazar054@gmail.com>
2022-10-31 14:49:19 -0600

    [UPD] Change name of foreign key

--- database/migrations/1667244151274_profiles.ts
+++ database/migrations/1667244151274_profiles.ts
@@ -6,7 +6,7 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
-      table.integer('profile_id').references('id')
+      table.integer('user_id').references('id')
         .inTable('users').unsigned().onDelete('CASCADE')
       table.string('name', 250).notNullable()
       table.string('lastname', 250).notNullable()
