9da959498565d1a8e006153525eabfd39f62253a
METR1CKA <ferchosalazar054@gmail.com>
2022-11-02 16:16:04 -0600

    [UPD] Change middlewares

--- app/Middleware/Auth.ts
+++ app/Middleware/Auth.ts
@@ -1,12 +1,21 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import MessagesI18n from 'App/Messages/MessagesI18n'
 
 export default class AuthMiddleware {
-  public async handle ({ auth, response }: HttpContextContract, next: () => Promise<void>) {
+  public async handle (ctx: HttpContextContract, next: () => Promise<void>) {
 
-    await auth.use('api').check()
+    const isLogged = await ctx.auth.use('api').check()
 
-    if (!auth.isAuthenticated) {
-      return response.unauthorized({})
+    if (!ctx.auth.isAuthenticated && ! isLogged) {
+      const obj = new MessagesI18n(ctx.request.header('Accept-language'))
+
+      const message = obj.format(
+        obj.messageA('messages.errors.noLogin'),
+        obj.messageA('messages.FAILED'),
+        null
+      )
+
+      return ctx.response.unauthorized(message)
     }
 
     await next()
--- app/Middleware/DetectUserLocale.ts
+++ app/Middleware/DetectUserLocale.ts
@@ -1,18 +1,25 @@
 import I18n from '@ioc:Adonis/Addons/I18n'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import MessagesI18n from 'App/Messages/MessagesI18n'
 
 export default class DetectUserLocale {
   protected getUserLanguage({ request }: HttpContextContract) {
-    const availableLocales = I18n.supportedLocales()
-
-    return request.language(availableLocales) || request.input('lang')
+    return request.language(I18n.supportedLocales()) || request.input('lang')
   }
 
   public async handle(ctx: HttpContextContract, next: () => Promise<void>) {
     const language = this.getUserLanguage(ctx)
 
     if (!language) {
-      return ctx.response.badRequest({})
+      const obj = new MessagesI18n(I18n.defaultLocale)
+
+      const message = obj.format(
+        obj.messageA('messages.errors.lang'),
+        obj.messageA('messages.FAILED'),
+        null
+      )
+
+      return ctx.response.badRequest(message)
     }
 
     ctx.i18n.switchLocale(language)
