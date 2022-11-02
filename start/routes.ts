bba8602f21d38d9357097cbf5bd2990ceb6ddc65
METR1CKA <ferchosalazar054@gmail.com>
2022-11-02 16:13:13 -0600

    [UPD] Routes added

--- start/routes.ts
+++ start/routes.ts
@@ -19,6 +19,8 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
+import MessagesI18n from 'App/Messages/MessagesI18n'
+import Env from '@ioc:Adonis/Core/Env'
 import './Auth/routes'
 import './Files/routes'
 import './Passwords/routes'
@@ -31,3 +33,35 @@ Route.get('/', async () => {
 Route.get('api/v1', async () => {
   return { API: 'V1' }
 })
+
+Route.get('api/v1/user', ({ response, auth, request }) => {
+  const obj = new MessagesI18n(request.header('Accept-language'))
+
+  return Env.get('NODE_ENV') === 'development'
+    ? response.ok(
+      obj.format(
+        obj.messageA('messages.success.authUser'),
+        obj.messageA('messages.SUCCESSFUL'),
+        auth.use('api').user
+      )
+    )
+    : response.badRequest(
+      obj.format(
+        obj.messageA('messages.errors.noProd'),
+        obj.messageA('messages.FAILED'),
+        null
+      )
+    )
+}).middleware(['lang', 'auth'])
+
+Route.any('*', ({ response, request }) => {
+  const obj = new MessagesI18n(request.header('Accept-language'))
+
+  const message = obj.format(
+    obj.messageA('messages.errors.route.notFound'),
+    obj.messageA('messages.FAILED'),
+    null
+  )
+
+  return response.notFound(message)
+}).middleware('lang')
