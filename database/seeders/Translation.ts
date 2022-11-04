7a20a30cec8632ab946a3bb7c306be9a0d186527
METR1CKA <ferchosalazar054@gmail.com>
2022-11-04 13:33:38 -0600

    [ADD] Translation Seeder Added

---
+++ database/seeders/Translation.ts
@@ -0,0 +1,22 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import Translation from 'App/Models/Translation'
+
+export default class extends BaseSeeder {
+  public async run () {
+    // Write your database queries inside the run method
+
+    await Translation.createMany([
+      { 'locale': 'en', 'key': 'user', 'message': 'User' },
+      { 'locale': 'es', 'key': 'user', 'message': 'Usuario' },
+
+      { 'locale': 'en', 'key': 'users', 'message': 'Users' },
+      { 'locale': 'es', 'key': 'users', 'message': 'Usuarios' },
+
+      { 'locale': 'en', 'key': 'role', 'message': 'Role' },
+      { 'locale': 'es', 'key': 'role', 'message': 'Rol' },
+
+      { 'locale': 'en', 'key': 'roles', 'message': 'Roles' },
+      { 'locale': 'es', 'key': 'roles', 'message': 'Roles' },
+    ])
+  }
+}
