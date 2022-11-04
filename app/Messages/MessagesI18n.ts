36f87cefed05ea08fdaffe5035bda8765f38fb60
METR1CKA <ferchosalazar054@gmail.com>
2022-11-04 13:37:24 -0600

    [UPD] Updated messages I18n

--- app/Messages/MessagesI18n.ts
+++ app/Messages/MessagesI18n.ts
@@ -1,18 +1,11 @@
 import I18n from '@ioc:Adonis/Addons/I18n'
 
 export default class MessagesI18n {
+
   private locale: any
 
   constructor (locale: any) {
     this.locale = locale
-  }
-
-  public format(message: string, status: string, data: any) {
-    return {
-      message,
-      status,
-      data
-    }
   }
 
   public messageA(key: string) {
@@ -23,28 +16,34 @@ export default class MessagesI18n {
     return I18n.locale(this.locale).formatMessage(key, data)
   }
 
+  public messageC(key: string, context: string) {
+    return I18n.locale(this.locale).formatMessage(key, {
+      data: I18n.locale(this.locale).formatMessage(context)
+    })
+  }
+
   public validationErr (Err: any) {
     const rule = Err.messages.errors[0].rule
     const field = Err.messages.errors[0].field
 
     if(rule == 'email') {
-      return this.messageA('')
+      return this.messageA('messages.errors.email')
     }
 
     if (rule == 'required') {
-      return this.messageB('', {field})
+      return this.messageB('messages.errors.field', {field})
     }
 
     if(rule == 'confirmed') {
-      return this.messageB('', {field})
+      return this.messageB('messages.errors.confirm', {field})
     }
 
     if (rule == 'maxLength') {
       const args = Err.messages.errors[0].args.maxLength
 
-      return this.messageB('', {field, args})
+      return this.messageB('messages.errors.maxLength', {field, args})
     }
 
-    return this.messageB('', {field, type: rule})
+    return this.messageB('messages.errors.rule', {field, type: rule})
   }
 }
