a52f93b658c13314e3866ad0e7dd0c7c180f6035
METR1CKA <ferchosalazar054@gmail.com>
2022-11-04 15:25:38 -0600

    [UPD] Middleware added in routes

--- start/Users/routes.ts
+++ start/Users/routes.ts
@@ -1,7 +1,18 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-  // Route.get('', async () => {
-  //   return { SERVER: 'ACTIVE' }
-  // })
+  //READ
+  Route.get('get/:id?', 'UsersController.get')
+
+  //CREATE
+  Route.post('create', 'UsersController.create')
+
+  //UPDATE
+  Route.put('update/:id?', 'UsersController.update')
+
+  //DELETE
+  Route.delete('delete/:id?', 'UsersController.delete')
 })
+  .prefix('api/v1/users')
+  .namespace('App/Controllers/Http/Users')
+  .middleware(['lang', 'auth', 'role_user'])
