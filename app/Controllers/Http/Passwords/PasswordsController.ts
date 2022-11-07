ee74210968d5465ddb12d1aea01ee0e11317be81
METR1CKA <ferchosalazar054@gmail.com>
2022-11-07 16:57:16 -0600

    [UPD] Password functions added

--- app/Controllers/Http/Passwords/PasswordsController.ts
+++ app/Controllers/Http/Passwords/PasswordsController.ts
@@ -1,17 +1,106 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import MessagesI18n from 'App/Messages/MessagesI18n'
+import User from 'App/Models/Users/User'
+import Hash from '@ioc:Adonis/Core/Hash'
+import PasswordAuthValidator from 'App/Validators/Passwords/PasswordAuthValidator'
+import PasswordIdValidator from 'App/Validators/Passwords/PasswordIdValidator'
 
 export default class PasswordsController {
-  public async index({}: HttpContextContract) {}
 
-  public async create({}: HttpContextContract) {}
+  public async updateAuth({ request, response, auth }: HttpContextContract) {
 
-  public async store({}: HttpContextContract) {}
+    const lang = new MessagesI18n(request.header('Accept-language'))
 
-  public async show({}: HttpContextContract) {}
+    const user = await User.find(auth.user?.id)
 
-  public async edit({}: HttpContextContract) {}
+    if (!user) {
+      return response.notFound({
+        message: lang.messageC('messages.errors.notFound', 'user'),
+        status: lang.messageA('messages.FAILED'),
+        data: null
+      })
+    }
 
-  public async update({}: HttpContextContract) {}
+    try {
 
-  public async destroy({}: HttpContextContract) {}
+      const vali = await request.validate(PasswordAuthValidator)
+
+      if (!vali) {
+        return
+      }
+
+      if (!await Hash.verify(user.password, vali.currentPassword)) {
+        return response.badRequest({
+          message: lang.messageA('messages.errors.password'),
+          status: lang.messageA('messages.FAILED'),
+          data: null
+        })
+      }
+
+      await user.merge({ password: vali.newPassword }).save()
+
+      return response.ok({
+        message: lang.messageC('messages.success.update', 'password'),
+        status: lang.messageA('messages.SUCCESSFUL'),
+        data: null
+      })
+
+    } catch (error) {
+
+      console.log(error)
+
+      return response.badRequest({
+        message: lang.validationErr(error),
+        status: lang.messageA('messages.FAILED'),
+        data: error.messages
+      })
+
+    }
+
+  }
+
+  public async updateId({ request, response, params }: HttpContextContract) {
+
+    const lang = new MessagesI18n(request.header('Accept-language'))
+
+    const user = await User.find(params.id)
+
+    if (!user) {
+      return response.notFound({
+        message: lang.messageC('messages.errors.notFound', 'user'),
+        status: lang.messageA('messages.FAILED'),
+        data: null
+      })
+    }
+
+    try {
+
+      const vali = await request.validate(PasswordIdValidator)
+
+      if (!vali) {
+        return
+      }
+
+      await user.merge({ password: vali.newPassword }).save()
+
+      return response.ok({
+        message: lang.messageC('messages.success.update', 'password'),
+        status: lang.messageA('messages.SUCCESSFUL'),
+        data: null
+      })
+
+    } catch (error) {
+
+      console.log(error)
+
+      return response.badRequest({
+        message: lang.validationErr(error),
+        status: lang.messageA('messages.FAILED'),
+        data: error.messages
+      })
+
+    }
+
+  }
+
 }
