1e57d3e616aa3b772132b98a6f289d44efa3e64e
METR1CKA <ferchosalazar054@gmail.com>
2022-11-07 13:00:10 -0600

    [UPD] Main routes changed

--- start/routes.ts
+++ start/routes.ts
@@ -36,34 +36,32 @@ Route.get('api/v1', async () => {
 
 Route.get('api/v1/user', async ({ response, auth, request }) => {
 
-  const obj = new MessagesI18n(request.header('Accept-language'))
+  const lang = new MessagesI18n(request.header('Accept-language'))
 
   const user = await User.findOrFail(auth.use('api').user?.id)
 
   return Env.get('NODE_ENV') === 'development'
     ? response.ok({
-      message: obj.messageA('messages.success.authUser'),
-      status: obj.messageA('messages.SUCCESSFUL'),
+      message: lang.messageA('messages.success.authUser'),
+      status: lang.messageA('messages.SUCCESSFUL'),
       data: user
     })
     : response.badRequest({
-      message: obj.messageA('messages.errors.noProd'),
-      status: obj.messageA('messages.FAILED'),
+      message: lang.messageA('messages.errors.noProd'),
+      status: lang.messageA('messages.FAILED'),
       data: null
     })
 
-})
-  .middleware(['lang', 'auth'])
+}).middleware(['lang', 'auth'])
 
 Route.any('*', ({ response, request }) => {
 
-  const obj = new MessagesI18n(request.header('Accept-language'))
+  const lang = new MessagesI18n(request.header('Accept-language'))
 
   return response.notFound({
-    message: obj.messageA('messages.errors.route.notFound'),
-    status: obj.messageA('messages.FAILED'),
+    message: lang.messageA('messages.errors.route.notFound'),
+    status: lang.messageA('messages.FAILED'),
     data: null
   })
 
-})
-  .middleware('lang')
+}).middleware('lang')
