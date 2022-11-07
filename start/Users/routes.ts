54fd6381b7f757e03814a3b5dff93422c2d54aa1
METR1CKA <ferchosalazar054@gmail.com>
2022-11-07 16:56:54 -0600

    [UPD] Routes added and others modificated

--- start/Users/routes.ts
+++ start/Users/routes.ts
@@ -1,58 +1,40 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-
+  // USERS
   Route.group(() => {
+    //READ
+    Route.get('get/:id?', 'UsersController.get')
+    //CREATE
+    Route.post('create', 'UsersController.create')
+    //UPDATE
+    Route.put('update/:id', 'UsersController.update')
+    //DELETE
+    Route.delete('delete/:id', 'UsersController.delete')
+  }).prefix('users')
 
-    // USERS
-
-    Route.group(() => {
-
-      //READ
-      Route.get('get/:id?', 'UsersController.get')
-
-      //CREATE
-      Route.post('create', 'UsersController.create')
-
-      //UPDATE
-      Route.put('update/:id', 'UsersController.update')
-
-      //DELETE
-      Route.delete('delete/:id', 'UsersController.delete')
-
-    }).prefix('users')
-
-    // ROLES
-
-    Route.group(() => {
-
-      //READ
-      Route.get('get/:id?', 'RolesController.get')
-
-      //CREATE
-      Route.post('create', 'RolesController.create')
-
-      //UPDATE
-      Route.put('update/:id', 'RolesController.update')
-
-      //DELETE
-      Route.delete('delete/:id', 'RolesController.delete')
-
-    }).prefix('roles')
-
-    // PASSWORDS
-
-    //UPDATE PASSWORD BY ID
-    Route.put('passwords/update/:id', 'PasswordsController.updateId')
-
-  }).middleware('role_user')
-
-  // PASSWORDS
-
-  //UPDATE PASSWORD AUTH USER
-  Route.get('passwords/update', 'PasswordsController.updateAuth')
-
+  // ROLES
+  Route.group(() => {
+    //READ
+    Route.get('get/:id?', 'RolesController.get')
+    //CREATE
+    Route.post('create', 'RolesController.create')
+    //UPDATE
+    Route.put('update/:id', 'RolesController.update')
+    //DELETE
+    Route.delete('delete/:id', 'RolesController.delete')
+  }).prefix('roles')
 })
   .prefix('api/v1')
   .namespace('App/Controllers/Http/Users')
+  .middleware(['lang', 'auth', 'role_user'])
+
+Route.group(() => {
+  // PASSWORDS
+
+  Route.put('update/:id', 'PasswordsController.updateId').middleware('role_user')
+  Route.put('update', 'PasswordsController.updateAuth')
+})
+  .prefix('api/v1/password')
+  .namespace('App/Controllers/Http/Passwords')
   .middleware(['lang', 'auth'])
