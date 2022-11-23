ca0cf4efe50b313feab2032cfed4617d4a8161cd
METR1CKA <ferchosalazar054@gmail.com>
2022-11-22 18:18:15 -0600

    [UPD] Change formats I18n

--- database/seeders/Translation.ts
+++ database/seeders/Translation.ts
@@ -6,20 +6,21 @@ export default class extends BaseSeeder {
     // Write your database queries inside the run method
 
     await Translation.createMany([
-      { 'locale': 'en', 'key': 'user', 'message': 'User' },
-      { 'locale': 'es', 'key': 'user', 'message': 'Usuario' },
+      { locale: 'en', key: 'user', message: 'User' },
+      { locale: 'es', key: 'user', message: 'Usuario' },
 
-      { 'locale': 'en', 'key': 'users', 'message': 'Users' },
-      { 'locale': 'es', 'key': 'users', 'message': 'Usuarios' },
+      { locale: 'en', key: 'users', message: 'Users' },
+      { locale: 'es', key: 'users', message: 'Usuarios' },
 
-      { 'locale': 'en', 'key': 'role', 'message': 'Role' },
-      { 'locale': 'es', 'key': 'role', 'message': 'Rol' },
+      { locale: 'en', key: 'role', message: 'Role' },
+      { locale: 'es', key: 'role', message: 'Rol' },
 
-      { 'locale': 'en', 'key': 'roles', 'message': 'Roles' },
-      { 'locale': 'es', 'key': 'roles', 'message': 'Roles' },
+      { locale: 'en', key: 'roles', message: 'Roles' },
+      { locale: 'es', key: 'roles', message: 'Roles' },
 
-      { 'locale': 'en', 'key': 'password', 'message': 'Password' },
-      { 'locale': 'es', 'key': 'password', 'message': 'Contraseña' },
+      { locale: 'en', key: 'password', message: 'Password' },
+      { locale: 'es', key: 'password', message: 'Contraseña' },
+
     ])
   }
 }
