b03115de0cd85fd5556d5aa570a356b9d332110c
METR1CKA <ferchosalazar054@gmail.com>
2022-12-19 16:44:13 -0600

    [UPD] Changes

--- app/Middleware/Auth.ts
+++ app/Middleware/Auth.ts
@@ -4,13 +4,9 @@ import MessagesI18n from 'App/Messages/MessagesI18n'
 export default class AuthMiddleware {
   public async handle (ctx: HttpContextContract, next: () => Promise<void>) {
 
-    const isCheck = await ctx.auth.use('api').check()
+    const isAuth = await ctx.auth.use('api').check()
 
-    const isAuthenticated = ctx.auth.isAuthenticated
-
-    const isLoggedIn = ctx.auth.isLoggedIn
-
-    if (!isCheck && !isAuthenticated && !isLoggedIn) {
+    if (!isAuth) {
       const obj = new MessagesI18n(ctx.request.header('Accept-language'))
 
       return ctx.response.unauthorized({
