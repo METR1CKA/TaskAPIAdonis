2fbea38a6574e312a8a37612a1b4640bff2838fd
METR1CKA <ferchosalazar054@gmail.com>
2023-01-16 11:34:19 -0600

    [UPD] Messages I18n

--- app/Messages/MessagesI18n.ts
+++ app/Messages/MessagesI18n.ts
@@ -9,40 +9,36 @@ export default class MessagesI18n {
    * A constructor function.
    * @param {any} locale - The locale to use.
    */
-  constructor (locale: any) {
+  constructor(locale: any) {
     this.locale = locale
   }
 
-  /**
-   * It takes a string as an argument, and returns a string
-   * @param {string} key - The key of the message to be translated.
-   * @returns The value of the key in the locale file.
-   */
-  public messageA(key: string) {
-    return I18n.locale(this.locale).formatMessage(key)
-  }
+  /* constructor() {
+    return this
+  } */
 
   /**
-   * It takes a key and data, and returns a formatted message
-   * @param {string} key - The key of the message to be translated.
-   * @param {any} data - The data object that will be used to replace the placeholders in the message.
+   * If the key and context are passed in, return the key with the context formatted. If the key and
+   * data are passed in, return the key with the data formatted. If only the key is passed in, return
+   * the key formatted
+   * @param {string} key - The key of the message you want to translate
+   * @param {string} [context] - The context of the message.
+   * @param {any} [data] - {
    * @returns The return value is a string.
    */
-  public messageB(key: string, data: any) {
-    return I18n.locale(this.locale).formatMessage(key, data)
-  }
+  public getMessage(key: string, context?: string, data?: any) {
 
-  /**
-   * It takes a key and a context, and returns the translation of the key, with the translation of the
-   * context as a data object
-   * @param {string} key - the key of the message you want to translate
-   * @param {string} context - The context of the message.
-   * @returns The value of the key in the locale file.
-   */
-  public messageC(key: string, context: string) {
-    return I18n.locale(this.locale).formatMessage(key, {
-      data: I18n.locale(this.locale).formatMessage(context)
-    })
+    if (key && context) {
+      return I18n.locale(this.locale).formatMessage(key, {
+        data: I18n.locale(this.locale).formatMessage(context)
+      })
+    }
+
+    if (key && data) {
+      return I18n.locale(this.locale).formatMessage(key, data)
+    }
+
+    return I18n.locale(this.locale).formatMessage(key)
   }
 
   /**
@@ -50,28 +46,28 @@ export default class MessagesI18n {
    * @param {any} Err - any - The error object that is returned from the server
    * @returns The return value is a string.
    */
-  public validationErr (Err: any) {
+  public validationErr(Err: any) {
     const rule = Err.messages.errors[0].rule
     const field = Err.messages.errors[0].field
 
-    if(rule == 'email') {
-      return this.messageA('messages.errors.email')
+    if (rule == 'email') {
+      return this.getMessage('email')
     }
 
     if (rule == 'required') {
-      return this.messageB('messages.errors.field', {field})
+      return this.getMessage('field', undefined, { field })
     }
 
-    if(rule == 'confirmed') {
-      return this.messageB('messages.errors.confirm', {field})
+    if (rule == 'confirmed') {
+      return this.getMessage('field.confirm', undefined, { field })
     }
 
     if (rule == 'maxLength') {
       const args = Err.messages.errors[0].args.maxLength
 
-      return this.messageB('messages.errors.maxLength', {field, args})
+      return this.getMessage('maxLength', undefined, { field, args })
     }
 
-    return this.messageB('messages.errors.rule', {field, type: rule})
+    return this.getMessage('field.type', undefined, { field, type: rule })
   }
 }
