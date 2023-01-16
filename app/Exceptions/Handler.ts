17d46b2398c5968b2ff814575ce0a24d05821f8a
METR1CKA <ferchosalazar054@gmail.com>
2023-01-16 12:48:33 -0600

    [UPD] Handler

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -15,9 +15,14 @@
 
 import Logger from '@ioc:Adonis/Core/Logger'
 import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
+import Env from '@ioc:Adonis/Core/Env'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
-  constructor () {
+  constructor() {
     super(Logger)
+  }
+
+  public devLogs(Err: any) {
+    Env.get('NODE_ENV') === 'development' ? console.log(Err) : null
   }
 }
