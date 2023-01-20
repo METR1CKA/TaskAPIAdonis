93ff64be21f7e365efcd51fa1aa5158a400399b9
METR1CKA <ferchosalazar054@gmail.com>
2023-01-19 19:59:28 -0600

    [UPD] File controller

--- app/Controllers/Http/Tasks/FilesController.ts
+++ app/Controllers/Http/Tasks/FilesController.ts
@@ -1,14 +1,12 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import MessagesI18n from 'App/Services/MessagesI18n'
-import Application from '@ioc:Adonis/Core/Application'
-import { DateTime } from 'luxon'
+import Drive from '@ioc:Adonis/Core/Drive'
+import Service from '@ioc:Adonis/Providers/Services'
 
 export default class FilesController {
-
   public header = 'Accept-Language'
 
-  public valids = {
-    size: '20mb',
+  public validations = {
+    size: '50mb',
     extnames: [
       'jpg',
       'png',
@@ -17,14 +15,31 @@ export default class FilesController {
       'svg',
       'mp4',
       'jfif',
-    ]
+      'pdf',
+      'bin',
+      'txt',
+      'doc',
+      'docx',
+      'avi',
+      'mp3',
+      'wav',
+      'zip',
+      'rar',
+      'tar',
+      'ico',
+      'xsl',
+      'xlsx',
+      'pptx',
+      'ppt',
+      'csv',
+      'rtf',
+    ],
   }
 
-  public async uploadFiles({ request, response, params }: HttpContextContract) {
+  public async uploadFiles({ request, response }: HttpContextContract) {
+    Service.locale = request.header(this.header)
 
-    const lang = new MessagesI18n(request.header(this.header))
-
-    const file = request.file('file', this.valids)
+    const file = request.file('file', this.validations)
 
     if (!file) {
       return
@@ -32,25 +47,61 @@ export default class FilesController {
 
     if (!file.isValid) {
       return response.badRequest({
-        message: lang.getMessage('', ''),
+        message: Service.getMessage('file.extnames'),
         data: {
           errors: file.errors
         }
       })
     }
 
-    const filename = 'File_' + DateTime.now()
+    const filename = await Service.storeFile(file, this.validations.extnames)
 
-    await file.move(Application.resourcesPath('uploads'), {
-      name: `${filename}.${file.extname}`,
-      overwrite: true
+    return response.created({
+      message: Service.getMessage('file'),
+      data: {
+        filename,
+      }
     })
-
-    return
   }
 
-  public async getFile({ }: HttpContextContract) {
-    //
+  public async getFile({ request, response, params }: HttpContextContract) {
+    Service.locale = request.header(this.header)
+
+    const filename: string = params.filename
+
+    if (!await Drive.exists(filename)) {
+      return response.notFound({
+        message: Service.getMessage('file.notExists'),
+        data: null
+      })
+    }
+
+    if (request.input('download')) {
+      return response.attachment(`resources/uploads/${filename}`)
+    }
+
+    const file = await Drive.getStream(filename)
+
+    return response.stream(file)
   }
 
+  public async deleteFile({ request, response, params }: HttpContextContract) {
+    Service.locale = request.header(this.header)
+
+    const filename: string = params.filename
+
+    if (!await Drive.exists(filename)) {
+      return response.notFound({
+        message: Service.getMessage('file.notExists'),
+        data: null
+      })
+    }
+
+    await Drive.delete(filename)
+
+    return response.ok({
+      message: Service.getMessage('file.deleted'),
+      data: null
+    })
+  }
 }
