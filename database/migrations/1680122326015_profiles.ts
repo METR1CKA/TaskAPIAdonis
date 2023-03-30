77697580b6a1603ddd512839abcd354eafa08039
METR1CKA <ferchosalazar054@gmail.com>
2023-03-30 13:18:53 -0600

    [UPD] Migrations updated

--- database/migrations/1680122326015_profiles.ts
+++ database/migrations/1680122326015_profiles.ts
@@ -8,17 +8,17 @@ export default class extends BaseSchema {
       table.increments('id').primary()
       table.integer('user_id').references('id').inTable('users').unsigned().onDelete('CASCADE')
       table.integer('lang_id').references('id').inTable('langs').unsigned().onDelete('CASCADE')
-      table.string('name', 250).notNullable()
-      table.string('lastname', 250).notNullable()
+      table.string('name', 200).notNullable()
+      table.string('lastname', 200).notNullable()
       table.string('phone', 10).nullable()
       table.text('address').nullable()
-      table.string('image', 250).nullable()
+      table.string('image', 200).nullable()
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+      // table.timestamp('created_at', { useTz: true })
+      // table.timestamp('updated_at', { useTz: true })
     })
   }
 
