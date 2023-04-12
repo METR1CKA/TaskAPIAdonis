95d90f0c0c84e91a62264d7bab78252a8058b84a
METR1CKA <ferchosalazar054@gmail.com>
2023-04-12 16:02:45 -0600

    [UPD] - Routes

--- start/Tasks/routes.ts
+++ start/Tasks/routes.ts
@@ -1,25 +1,19 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-
   Route.group(() => {
-
     Route.get('get/:filename', 'FilesController.getFile')
     Route.post('upload', 'FilesController.uploadFile')
     Route.delete('delete/:filename', 'FilesController.deleteFile')
-
   }).prefix('files')
 
   Route.group(() => {
-
     Route.get('get', 'TasksController.read')
     Route.get('get/:id', 'TasksController.read')
     Route.post('create', 'TasksController.create')
     Route.put('update/:id', 'TasksController.update')
     Route.delete('delete/:id', 'TasksController.delete')
-
   }).prefix('tasks')
-
 })
   .prefix('api/v1')
   .namespace('App/Controllers/Http/Tasks')
