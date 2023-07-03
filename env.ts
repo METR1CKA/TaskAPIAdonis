105fc88f5aac7eff34ca957921b38e82a8c36eb1
METR1CKA <ferchosalazar054@gmail.com>
2023-07-03 13:43:17 -0600

    [UPD] - Env added

--- env.ts
+++ env.ts
@@ -15,11 +15,12 @@
 import Env from '@ioc:Adonis/Core/Env'
 
 export default Env.rules({
-	HOST: Env.schema.string({ format: 'host' }),
-	PORT: Env.schema.number(),
-	APP_KEY: Env.schema.string(),
-	APP_NAME: Env.schema.string(),
-	DRIVE_DISK: Env.schema.enum(['local'] as const),
-	NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
-	DB_I18n: Env.schema.boolean()
+  HOST: Env.schema.string({ format: 'host' }),
+  PORT: Env.schema.number(),
+  APP_KEY: Env.schema.string(),
+  APP_NAME: Env.schema.string(),
+  DRIVE_DISK: Env.schema.enum(['local'] as const),
+  NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
+  DB_I18n: Env.schema.boolean(),
+  PASSWORD_DEFAULT: Env.schema.string()
 })
